d1dce35cab5429977b505c0146980019e6dc4955
Minor Gordon <minorg@server.fake>
2025-09-20 18:22:11 -0400

    cache some commonly-used types

--- examples/kitchen-sink/generate.sh
+++ examples/kitchen-sink/generate.sh
@@ -11,4 +11,4 @@ format_rdf() {
 
 format_rdf src/kitchen-sink.shaclmate.ttl
 #../../packages/cli/dist/cli.js show-ast-json src/kitchen-sink.shaclmate.ttl >src/ast.json
-../../packages/cli/dist/cli.js generate src/kitchen-sink.shaclmate.ttl >src/generated.ts
+time ../../packages/cli/dist/cli.js generate src/kitchen-sink.shaclmate.ttl >src/generated.ts
