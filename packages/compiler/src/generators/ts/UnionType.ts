0bd61d35775d651a1b9afde186d77916fe43543b
Minor Gordon <minorg@server.fake>
2025-09-23 12:36:22 -0400

    fix union types

--- packages/compiler/src/generators/ts/UnionType.ts
+++ packages/compiler/src/generators/ts/UnionType.ts
@@ -338,7 +338,7 @@ ${this.memberTypes
       if (this._discriminator.kind === "syntheticProperty") {
         typeExpression = `${typeExpression}.map(value => ({ ${this._discriminator.name}: "${memberType.discriminatorValues[0]}" as const, value }) as (${this.name}))`;
       }
-      typeExpression = `(${typeExpression} as purify.Either<Error, ${this.name}>)`;
+      typeExpression = `(${typeExpression} as purify.Either<Error, rdfjsResource.Resource.Values<${this.name}>>)`;
       return expression.length > 0
         ? `${expression}.altLazy(() => ${typeExpression})`
         : typeExpression;
