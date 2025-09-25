a76d82a52d6cc1897d9c896c8ee6e26e029a841a
Minor Gordon <minorg@server.fake>
2025-09-25 11:56:40 -0400

    bump version

--- generate-package-files.ts
+++ generate-package-files.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import url from "node:url";
 import { stringify as stringifyYaml } from "yaml";
 
-const VERSION = "2.0.23";
+const VERSION = "2.0.24";
 
 type PackageName =
   | "cli"
