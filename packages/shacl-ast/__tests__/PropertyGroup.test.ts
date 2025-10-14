d829ed1472bb0b5daf51f104ec3d8b6252ed7ca6
Minor Gordon <minorg@server.fake>
2025-10-14 14:45:31 -0400

    remove empty imports

--- packages/shacl-ast/__tests__/PropertyGroup.test.ts
+++ packages/shacl-ast/__tests__/PropertyGroup.test.ts
@@ -1,6 +1,5 @@
 import { dash } from "@tpluscode/rdf-ns-builders";
 import { describe, it } from "vitest";
-import {} from "../src/index.js";
 import { testData } from "./testData.js";
 
 describe("PropertyGroup", () => {
