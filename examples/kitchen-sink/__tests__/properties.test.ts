5f0db9f9d0e6d6bbe535375ee65fc325241ccbb4
Minor Gordon <minorg@server.fake>
2025-11-22 10:00:05 -0500

    start of properties test

---
+++ examples/kitchen-sink/__tests__/properties.test.ts
@@ -0,0 +1,48 @@
+import { describe, it } from "vitest";
+import * as kitchenSink from "../src/index.js";
+
+describe("properties", () => {
+  it("class properties should have an own property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildClass.$properties.concreteChildClassProperty
+        .identifier.value,
+    ).toStrictEqual("http://example.com/concreteChildClassProperty");
+  });
+
+  it("class properties should have a parent property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildClass.$properties.concreteParentClassProperty
+        .identifier.value,
+    ).toStrictEqual("http://example.com/concreteParentClassProperty");
+  });
+
+  it("class properties should have an ancestor property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildClass.$properties
+        .abstractBaseClassWithPropertiesProperty.identifier.value,
+    ).toStrictEqual(
+      "http://example.com/abstractBaseClassWithPropertiesProperty",
+    );
+  });
+
+  it("interface properties should have an own property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildInterface.$properties
+        .concreteChildInterfaceProperty.identifier.value,
+    ).toStrictEqual("http://example.com/concreteChildInterfaceProperty");
+  });
+
+  it("interface properties should have a parent property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildInterface.$properties
+        .concreteParentInterfaceProperty.identifier.value,
+    ).toStrictEqual("http://example.com/concreteParentInterfaceProperty");
+  });
+
+  it("class properties should have an ancestor property", ({ expect }) => {
+    expect(
+      kitchenSink.ConcreteChildInterface.$properties
+        .baseInterfaceWithPropertiesProperty.identifier.value,
+    ).toStrictEqual("http://example.com/baseInterfaceWithPropertiesProperty");
+  });
+});
