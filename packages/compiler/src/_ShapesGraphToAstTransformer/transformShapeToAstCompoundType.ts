b36bc93edc5b8466e19a7c025645b41dd8b612e8
Minor Gordon <minorg@server.fake>
2026-01-03 17:09:20 -0500

    reworking

--- packages/compiler/src/_ShapesGraphToAstTransformer/transformShapeToAstCompoundType.ts
+++ packages/compiler/src/_ShapesGraphToAstTransformer/transformShapeToAstCompoundType.ts
@@ -1,9 +1,9 @@
-import { Eithers } from "Eithers.js";
 import type {} from "@shaclmate/shacl-ast";
 import { owl, rdfs } from "@tpluscode/rdf-ns-builders";
 import { Either, Left, Maybe } from "purify-ts";
 import { invariant } from "ts-invariant";
 import * as ast from "../ast/index.js";
+import { Eithers } from "../Eithers.js";
 import * as input from "../input/index.js";
 import type { ShapesGraphToAstTransformer } from "../ShapesGraphToAstTransformer.js";
 import type { ShapeStack } from "./ShapeStack.js";
