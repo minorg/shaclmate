8dcf0957012ae2246f1e45e39c1a919f4c0699c2
Minor Gordon <minorg@server.fake>
2026-01-28 11:51:14 -0500

    expand objectInitializer

--- packages/compiler/src/generators/ts/objectInitializer.ts
+++ packages/compiler/src/generators/ts/objectInitializer.ts
@@ -1,15 +1,56 @@
-export function objectInitializer(
-  properties: Record<string, boolean | number | string | undefined>,
-): string {
+interface Properties {
+  [key: string]: PropertyValue;
+}
+
+type PropertyValue =
+  | boolean
+  | null
+  | number
+  | string
+  | undefined
+  | Properties
+  | PropertyValue[];
+
+function propertyInitializer(propertyValue: PropertyValue): string | undefined {
+  switch (typeof propertyValue) {
+    case "object":
+      if (propertyValue === null) {
+        return "null";
+      }
+
+      if (Array.isArray(propertyValue)) {
+        return `[${propertyValue
+          .map(propertyInitializer)
+          .filter((_) => typeof _ !== "undefined")
+          .join(", ")}]`;
+      }
+
+      return objectInitializer(propertyValue);
+    case "undefined":
+      return undefined;
+    default:
+      return propertyValue.toString();
+  }
+}
+
+/**
+ * Convert a compile-time, JSON-compatible TypeScript object to a TypeScript object initializer that can be used at runtime.
+ *
+ * Uses shorthand properties and doesn't quote strings like JSON.stringify would.
+ */
+export function objectInitializer(properties: Properties): string {
   return `{ ${Object.entries(properties)
     .flatMap(([propertyName, propertyValue]) => {
-      if (typeof propertyValue === "undefined") {
+      if (typeof propertyValue === "string" && propertyName === propertyValue) {
+        return [propertyName]; // Shorthand
+      }
+
+      const propertyInitializer_ = propertyInitializer(propertyValue);
+      if (typeof propertyInitializer_ === "undefined") {
         return [];
       }
-      if (typeof propertyValue === "string" && propertyName === propertyValue) {
-        return [propertyName];
-      }
-      return [`${propertyName}: ${propertyValue}`];
+
+      return [`${propertyName}: ${propertyInitializer_}`];
     })
     .join(", ")} }`;
 }
