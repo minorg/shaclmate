794a3634706032de19d5802b815b40bf32e4feae
Minor Gordon <minorg@server.fake>
2026-02-09 18:38:10 -0500

    BlankNodeType

--- packages/compiler/src/generators/ts/BlankNodeType.ts
+++ packages/compiler/src/generators/ts/BlankNodeType.ts
@@ -1,14 +1,20 @@
 import type { BlankNode, NamedNode } from "@rdfjs/types";
-import { type FunctionDeclarationStructure, StructureKind } from "ts-morph";
+import { type Code, code, conditionalOutput } from "ts-poet";
 import { Memoize } from "typescript-memoize";
 import { AbstractIdentifierType } from "./AbstractIdentifierType.js";
 import { AbstractTermType } from "./AbstractTermType.js";
-import { mergeSnippetDeclarations } from "./mergeSnippetDeclarations.js";
-import { objectInitializer } from "./objectInitializer.js";
-import type { SnippetDeclaration } from "./SnippetDeclaration.js";
-import { sharedSnippetDeclarations } from "./sharedSnippets.js";
-import { singleEntryRecord } from "./singleEntryRecord.js";
+import { sharedImports } from "./sharedImports.js";
+import { sharedSnippets } from "./sharedSnippets.js";
 import { syntheticNamePrefix } from "./syntheticNamePrefix.js";
+
+const localSnippets = {
+  BlankNodeFilter: conditionalOutput(
+    `${syntheticNamePrefix}BlankNodeFilter`,
+    code`\
+interface ${syntheticNamePrefix}BlankNodeFilter {
+}`,
+  ),
+};
 
 const nodeKinds: ReadonlySet<"BlankNode"> = new Set(["BlankNode"]);
 
@@ -31,60 +37,51 @@ export class BlankNodeType extends AbstractIdentifierType<BlankNode> {
 
   @Memoize()
   get filterFunction() {
-    return `${syntheticNamePrefix}filterBlankNode`;
+    return code`${conditionalOutput(
+      `${syntheticNamePrefix}filterBlankNode`,
+      code`\
+function ${syntheticNamePrefix}filterBlankNode(_filter: ${localSnippets.BlankNodeFilter}, _value: ${sharedImports.BlankNode}) {
+  return true;
+}`,
+    )}`;
   }
 
   @Memoize()
-  get filterType(): string {
-    return `${syntheticNamePrefix}BlankNodeFilter`;
+  get filterType(): Code {
+    return code`${localSnippets.BlankNodeFilter}`;
   }
 
-  override get name(): string {
-    return "rdfjs.BlankNode";
-  }
-
-  @Memoize()
-  override get schema(): string {
-    if (this.constrained) {
-      return objectInitializer(this.schemaObject);
-    }
-
-    return `${syntheticNamePrefix}blankNodeIdentifierTypeSchema`;
+  override get name(): Code {
+    return code`${sharedImports.BlankNode}`;
   }
 
   @Memoize()
-  override get sparqlWherePatternsFunction(): string {
-    return `${syntheticNamePrefix}blankNodeSparqlWherePatterns`;
+  override get sparqlWherePatternsFunction(): Code {
+    return code`${conditionalOutput(
+      `${syntheticNamePrefix}blankNodeSparqlWherePatterns`,
+      code`\
+const ${syntheticNamePrefix}blankNodeSparqlWherePatterns: ${sharedSnippets.SparqlWherePatternsFunction}<${this.filterType}, ${this.schemaType}> =
+  ({ propertyPatterns }) => propertyPatterns;`,
+    )}`;
   }
 
   override fromJsonExpression({
     variables,
   }: Parameters<
     AbstractTermType<NamedNode, BlankNode | NamedNode>["fromJsonExpression"]
-  >[0]): string {
-    return `dataFactory.blankNode(${variables.value}["@id"].substring(2))`;
+  >[0]): Code {
+    return code`${sharedImports.dataFactory}.blankNode(${variables.value}["@id"].substring(2))`;
   }
 
   @Memoize()
-  get fromStringFunctionDeclaration(): FunctionDeclarationStructure {
-    return {
-      isExported: true,
-      kind: StructureKind.Function,
-      name: "fromString",
-      parameters: [
-        {
-          name: "identifier",
-          type: "string",
-        },
-      ],
-      returnType: `purify.Either<Error, ${this.name}>`,
-      statements: [
-        `return ${[
-          "purify.Either.encase(() => rdfjsResource.Resource.Identifier.fromString({ dataFactory, identifier }))",
-          `chain((identifier) => (identifier.termType === "BlankNode") ? purify.Either.of(identifier) : purify.Left(new Error("expected identifier to be BlankNode")))`,
-        ].join(".")} as purify.Either<Error, ${this.name}>;`,
-      ],
-    };
+  get fromStringFunctionDeclaration(): Code {
+    return code`\
+export function fromString(identifier: string): ${sharedImports.Either}<Error, ${this.name}> {
+    return \
+      ${sharedImports.Either}.encase(() => ${sharedImports.Resource}.Identifier.fromString({ ${sharedImports.dataFactory}, identifier }))
+      .chain((identifier) => (identifier.termType === "BlankNode") ? ${sharedImports.Either}.of(identifier) : ${sharedImports.Left}(new Error("expected identifier to be BlankNode")))
+      as ${sharedImports.Either}<Error, ${this.name}>;
+}`;
   }
 
   @Memoize()
@@ -96,7 +93,7 @@ export class BlankNodeType extends AbstractIdentifierType<BlankNode> {
       : "";
 
     return new AbstractTermType.JsonType(
-      `{ readonly "@id": string${discriminantProperty} }`,
+      code`{ readonly "@id": string${discriminantProperty} }`,
     );
   }
 
@@ -105,57 +102,12 @@ export class BlankNodeType extends AbstractIdentifierType<BlankNode> {
     variables,
   }: Parameters<
     AbstractTermType<NamedNode, BlankNode | NamedNode>["jsonZodSchema"]
-  >[0]): ReturnType<
-    AbstractTermType<NamedNode, BlankNode | NamedNode>["jsonZodSchema"]
-  > {
+  >[0]): Code {
     const discriminantProperty = includeDiscriminantProperty
       ? `, termType: ${variables.zod}.literal("BlankNode")`
       : "";
 
-    return `${variables.zod}.object({ "@id": ${variables.zod}.string().min(1)${discriminantProperty} })`;
-  }
-
-  override snippetDeclarations(
-    parameters: Parameters<AbstractTermType["snippetDeclarations"]>[0],
-  ): Readonly<Record<string, SnippetDeclaration>> {
-    const { features } = parameters;
-
-    return mergeSnippetDeclarations(
-      super.snippetDeclarations(parameters),
-
-      singleEntryRecord(
-        `${syntheticNamePrefix}BlankNodeFilter`,
-        `\
-interface ${syntheticNamePrefix}BlankNodeFilter {
-}`,
-      ),
-
-      singleEntryRecord(
-        `${syntheticNamePrefix}filterBlankNode`,
-        `\
-function ${syntheticNamePrefix}filterBlankNode(_filter: ${syntheticNamePrefix}BlankNodeFilter, _value: rdfjs.BlankNode) {
-  return true;
-}`,
-      ),
-
-      features.has("sparql")
-        ? singleEntryRecord(
-            `${syntheticNamePrefix}blankNodeSparqlWherePatterns`,
-            {
-              code: `\
-const ${syntheticNamePrefix}blankNodeSparqlWherePatterns: ${syntheticNamePrefix}SparqlWherePatternsFunction<${this.filterType}, ${this.schemaType}> =
-  ({ propertyPatterns }) => propertyPatterns;`,
-              dependencies:
-                sharedSnippetDeclarations.SparqlWherePatternsFunction,
-            },
-          )
-        : {},
-
-      singleEntryRecord(
-        `${syntheticNamePrefix}blankNodeIdentifierTypeSchema`,
-        `const ${syntheticNamePrefix}blankNodeIdentifierTypeSchema = ${objectInitializer(this.schemaObject)};`,
-      ),
-    );
+    return code`${variables.zod}.object({ "@id": ${variables.zod}.string().min(1)${discriminantProperty} })`;
   }
 
   override toJsonExpression({
@@ -163,21 +115,19 @@ const ${syntheticNamePrefix}blankNodeSparqlWherePatterns: ${syntheticNamePrefix}
     variables,
   }: Parameters<
     AbstractTermType<NamedNode, BlankNode | NamedNode>["toJsonExpression"]
-  >[0]): string {
+  >[0]): Code {
     const discriminantProperty = includeDiscriminantProperty
       ? `, termType: ${variables.value}.termType`
       : "";
-    return `{ "@id": \`_:\${${variables.value}.value}\`${discriminantProperty} }`;
+    return code`{ "@id": \`_:\${${variables.value}.value}\`${discriminantProperty} }`;
   }
 
   protected override fromRdfExpressionChain({
     variables,
-  }: Parameters<AbstractTermType["fromRdfExpressionChain"]>[0]): ReturnType<
-    AbstractTermType["fromRdfExpressionChain"]
-  > {
+  }: Parameters<AbstractTermType["fromRdfExpressionChain"]>[0]) {
     return {
       ...super.fromRdfExpressionChain({ variables }),
-      valueTo: `chain(values => values.chainMap(value => value.toBlankNode()))`,
+      valueTo: code`chain(values => values.chainMap(value => value.toBlankNode()))`,
     };
   }
 }
