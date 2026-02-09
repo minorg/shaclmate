f881d3643084716a720173a1849c6b3be7537b84
Minor Gordon <minorg@server.fake>
2026-02-09 17:18:35 -0500

    delete singleEntryRecord

--- packages/compiler/src/generators/ts/singleEntryRecord.ts
+++
@@ -1,8 +0,0 @@
-export function singleEntryRecord<ValueT>(
-  key: string,
-  value: ValueT,
-): Record<string, ValueT> {
-  const result: Record<string, ValueT> = {};
-  result[key] = value;
-  return result;
-}
