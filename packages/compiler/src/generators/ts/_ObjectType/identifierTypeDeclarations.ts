b42abcbe629ac0cf4177abdb4a5f7016c52809d0
Minor Gordon <minorg@server.fake>
2026-02-12 12:58:51 -0500

    identifierTypeDeclarations

--- packages/compiler/src/generators/ts/_ObjectType/identifierTypeDeclarations.ts
+++ packages/compiler/src/generators/ts/_ObjectType/identifierTypeDeclarations.ts
@@ -1,24 +1,8 @@
-import {
-  type FunctionDeclarationStructure,
-  type ModuleDeclarationStructure,
-  StructureKind,
-  type TypeAliasDeclarationStructure,
-  VariableDeclarationKind,
-  type VariableStatementStructure,
-} from "ts-morph";
+import { type Code, code, joinCode } from "ts-poet";
 import type { ObjectType } from "../ObjectType.js";
 import { syntheticNamePrefix } from "../syntheticNamePrefix.js";
 
-type IdentifierTypeDeclarations = readonly (
-  | FunctionDeclarationStructure
-  | ModuleDeclarationStructure
-  | TypeAliasDeclarationStructure
-  | VariableStatementStructure
-)[];
-
-export function identifierTypeDeclarations(
-  this: ObjectType,
-): IdentifierTypeDeclarations {
+export function identifierTypeDeclarations(this: ObjectType): readonly Code[] {
   const ancestorObjectTypeWithSameIdentifierType =
     this.ancestorObjectTypes.find(
       (ancestorObjectType) =>
@@ -26,53 +10,17 @@ export function identifierTypeDeclarations(
     );
 
   if (ancestorObjectTypeWithSameIdentifierType) {
-    return reExportAncestorIdentifierTypeDeclarations(
-      ancestorObjectTypeWithSameIdentifierType,
-    );
+    // This object type's identifier type has the same identifier type as an ancestor object type,
+    // so just reuse the latter.
+    return [
+      code`export type ${syntheticNamePrefix}Identifier = ${ancestorObjectTypeWithSameIdentifierType.identifierTypeAlias};`,
+      code`export const ${syntheticNamePrefix}Identifier = ${ancestorObjectTypeWithSameIdentifierType.identifierTypeAlias};`,
+    ];
   }
 
   // Bespoke identifier type and associated functions
   return [
-    {
-      isExported: true,
-      kind: StructureKind.TypeAlias,
-      name: `${syntheticNamePrefix}Identifier`,
-      type: this.identifierType.name,
-    },
-    {
-      isExported: true,
-      kind: StructureKind.Module,
-      name: `${syntheticNamePrefix}Identifier`,
-      statements: [
-        this.identifierType.fromStringFunction,
-        this.identifierType.toStringFunction,
-      ],
-    },
-  ];
-}
-
-function reExportAncestorIdentifierTypeDeclarations(
-  ancestorObjectType: ObjectType,
-): IdentifierTypeDeclarations {
-  // This object type's identifier type has the same identifier type as an ancestor object type,
-  // so just reuse the latter.
-  return [
-    {
-      isExported: true,
-      kind: StructureKind.TypeAlias,
-      name: `${syntheticNamePrefix}Identifier`,
-      type: ancestorObjectType.identifierTypeAlias,
-    },
-    {
-      isExported: true,
-      kind: StructureKind.VariableStatement,
-      declarationKind: VariableDeclarationKind.Const,
-      declarations: [
-        {
-          initializer: ancestorObjectType.identifierTypeAlias,
-          name: `${syntheticNamePrefix}Identifier`,
-        },
-      ],
-    },
+    code`export type ${syntheticNamePrefix}Identifier = ${this.identifierType.name};`,
+    code`export namespace ${syntheticNamePrefix}Identifier { ${joinCode([this.identifierType.fromStringFunction, this.identifierType.toStringFunction])} }`,
   ];
 }
