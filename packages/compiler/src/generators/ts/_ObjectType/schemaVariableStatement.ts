5ad29c8c3675bb8e332fcf36ec194a0a44c60456
Minor Gordon <minorg@server.fake>
2026-02-12 14:07:56 -0500

    schemaVariableStatement

--- packages/compiler/src/generators/ts/_ObjectType/schemaVariableStatement.ts
+++ packages/compiler/src/generators/ts/_ObjectType/schemaVariableStatement.ts
@@ -1,34 +1,21 @@
-import {
-  StructureKind,
-  VariableDeclarationKind,
-  type VariableStatementStructure,
-} from "ts-morph";
-
+import { type Code, code, joinCode } from "ts-poet";
 import type { ObjectType } from "../ObjectType.js";
 import { syntheticNamePrefix } from "../syntheticNamePrefix.js";
 
-export function schemaVariableStatement(
-  this: ObjectType,
-): VariableStatementStructure {
-  return {
-    declarationKind: VariableDeclarationKind.Const,
-    kind: StructureKind.VariableStatement,
-    declarations: [
-      {
-        name: `${syntheticNamePrefix}schema`,
-        initializer: `{ properties: { ${this.parentObjectTypes
-          .map(
-            (parentObjectType) =>
-              `...${parentObjectType.staticModuleName}.${syntheticNamePrefix}schema.properties`,
-          )
-          .concat(
-            this.ownProperties.map(
-              (property) => `${property.name}: ${property.schema}`,
-            ),
-          )
-          .join(", ")} } } as const`,
-      },
-    ],
-    isExported: true,
-  };
+export function schemaVariableStatement(this: ObjectType): Code {
+  return code`\
+export const ${syntheticNamePrefix}Schema = { properties: { ${joinCode(
+    this.parentObjectTypes
+      .map(
+        (parentObjectType) =>
+          code`...${parentObjectType.staticModuleName}.${syntheticNamePrefix}schema.properties`,
+      )
+      .concat(
+        this.ownProperties.map(
+          (property) => code`${property.name}: ${property.schema}`,
+        ),
+      ),
+    { on: ", " },
+  )} } as const;
+`;
 }
