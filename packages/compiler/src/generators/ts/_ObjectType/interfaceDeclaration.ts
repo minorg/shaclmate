0d7316f06b21a4b237d525d8508c12474005503a
Minor Gordon <minorg@server.fake>
2026-02-12 13:04:15 -0500

    interfaceDeclaration

--- packages/compiler/src/generators/ts/_ObjectType/interfaceDeclaration.ts
+++ packages/compiler/src/generators/ts/_ObjectType/interfaceDeclaration.ts
@@ -1,31 +1,28 @@
 import { Maybe } from "purify-ts";
-import {
-  type InterfaceDeclarationStructure,
-  type OptionalKind,
-  type PropertySignatureStructure,
-  StructureKind,
-} from "ts-morph";
+import { type Code, code, joinCode } from "ts-poet";
 import type { ObjectType } from "../ObjectType.js";
 import { tsComment } from "../tsComment.js";
 
-export function interfaceDeclaration(
-  this: ObjectType,
-): Maybe<InterfaceDeclarationStructure> {
+export function interfaceDeclaration(this: ObjectType): Maybe<Code> {
   if (this.declarationType !== "interface") {
     return Maybe.empty();
   }
 
-  const properties: OptionalKind<PropertySignatureStructure>[] =
-    this.properties.flatMap((property) => property.propertySignature.toList());
-
-  return Maybe.of({
-    extends: this.parentObjectTypes.map(
-      (parentObjectType) => parentObjectType.name,
-    ),
-    isExported: true,
-    kind: StructureKind.Interface,
-    leadingTrivia: this.comment.alt(this.label).map(tsComment).extract(),
-    name: this.name,
-    properties,
-  });
+  return Maybe.of(code`\
+${this.comment
+  .alt(this.label)
+  .map(tsComment)
+  .map((comment) => `/* ${comment} */`)
+  .orDefault("")}
+export interface ${this.name}${
+    this.parentObjectTypes.length > 0
+      ? `extends ${this.parentObjectTypes
+          .map((parentObjectType) => parentObjectType.name)
+          .join(", ")}`
+      : ""
+  } {
+  ${joinCode(
+    this.properties.flatMap((property) => property.declaration.toList()),
+  )}
+}`);
 }
