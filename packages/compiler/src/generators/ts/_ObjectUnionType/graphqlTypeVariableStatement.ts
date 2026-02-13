6597729421098a1ffe8e9b860af5a7955210dad7
Minor Gordon <minorg@server.fake>
2026-02-12 19:30:11 -0500

    graphqlTypeVariableStatement

--- packages/compiler/src/generators/ts/_ObjectUnionType/graphqlTypeVariableStatement.ts
+++ packages/compiler/src/generators/ts/_ObjectUnionType/graphqlTypeVariableStatement.ts
@@ -1,34 +1,26 @@
 import { Maybe } from "purify-ts";
-import {
-  StructureKind,
-  VariableDeclarationKind,
-  type VariableStatementStructure,
-} from "ts-morph";
+import { type Code, code, joinCode, literalOf } from "ts-poet";
 import type { ObjectUnionType } from "../ObjectUnionType.js";
-import { objectInitializer } from "../objectInitializer.js";
 import { syntheticNamePrefix } from "../syntheticNamePrefix.js";
 
 export function graphqlTypeVariableStatement(
   this: ObjectUnionType,
-): Maybe<VariableStatementStructure> {
+): Maybe<Code> {
   if (!this.features.has("graphql")) {
     return Maybe.empty();
   }
 
-  return Maybe.of({
-    declarationKind: VariableDeclarationKind.Const,
-    kind: StructureKind.VariableStatement,
-    declarations: [
-      {
-        name: `${syntheticNamePrefix}GraphQL`,
-        initializer: `new graphql.GraphQLUnionType(${objectInitializer({
-          description: this.comment.map(JSON.stringify).extract(),
-          name: `"${this.name}"`,
-          resolveType: `(value: ${this.name}) => value.${syntheticNamePrefix}type`,
-          types: `[${this.concreteMemberTypes.map((memberType) => memberType.graphqlType.nullableName).join(", ")}]`,
-        })})`,
-      },
-    ],
-    isExported: true,
-  } satisfies VariableStatementStructure);
+  return Maybe.of(code`\
+export const ${syntheticNamePrefix}GraphQL = new graphql.GraphQLUnionType(${{
+    description: this.comment.map(JSON.stringify).extract(),
+    name: literalOf(this.nameString),
+    resolveType: code`(value: ${this.name}) => value.${syntheticNamePrefix}type`,
+    types: code`[${joinCode(
+      this.concreteMemberTypes.map(
+        (memberType) => memberType.graphqlType.nullableName,
+      ),
+      { on: ", " },
+    )}]`,
+  }});
+`);
 }
