3f679d8eb1aeefaf1affeab7274bf140b47ce2a1
Minor Gordon <minorg@server.fake>
2026-02-13 12:09:31 -0500

    objectSetMethodSignatures

--- packages/compiler/src/generators/ts/objectSetMethodSignatures.ts
+++ packages/compiler/src/generators/ts/objectSetMethodSignatures.ts
@@ -1,68 +1,26 @@
 import type { MethodSignatureStructure, OptionalKind } from "ts-morph";
+import { type Code, code } from "ts-poet";
 import type { ObjectType } from "./ObjectType.js";
+import { sharedImports } from "./sharedImports.js";
 import { syntheticNamePrefix } from "./syntheticNamePrefix.js";
 
 export function objectSetMethodSignatures(parameters: {
   objectType: {
-    readonly filterType: string;
-    readonly identifierTypeAlias: string;
+    readonly filterType: Code;
+    readonly identifierTypeAlias: Code;
     readonly objectSetMethodNames: ObjectType.ObjectSetMethodNames;
     readonly name: string;
   };
   queryT?: string;
-}): Readonly<
-  Record<
-    keyof ObjectType.ObjectSetMethodNames,
-    OptionalKind<MethodSignatureStructure>
-  >
-> {
+}): Readonly<Record<keyof ObjectType.ObjectSetMethodNames, Code>> {
   const { objectType } = parameters;
   const queryT = parameters.queryT ?? `${syntheticNamePrefix}ObjectSet.Query`;
 
   const methodNames = objectType.objectSetMethodNames;
   return {
-    object: {
-      name: methodNames.object,
-      parameters: [
-        {
-          name: "identifier",
-          type: objectType.identifierTypeAlias,
-        },
-      ],
-      returnType: `Promise<purify.Either<Error, ${objectType.name}>>`,
-    },
-    objectIdentifiers: {
-      name: methodNames.objectIdentifiers,
-      parameters: [
-        {
-          hasQuestionToken: true,
-          name: "query",
-          type: `${queryT}<${objectType.filterType}>`,
-        },
-      ],
-      returnType: `Promise<purify.Either<Error, readonly ${objectType.identifierTypeAlias}[]>>`,
-    },
-    objects: {
-      name: methodNames.objects,
-      parameters: [
-        {
-          hasQuestionToken: true,
-          name: "query",
-          type: `${queryT}<${objectType.filterType}>`,
-        },
-      ],
-      returnType: `Promise<purify.Either<Error, readonly ${objectType.name}[]>>`,
-    },
-    objectsCount: {
-      name: methodNames.objectsCount,
-      parameters: [
-        {
-          hasQuestionToken: true,
-          name: "query",
-          type: `Pick<${queryT}<${objectType.filterType}>, "filter">`,
-        },
-      ],
-      returnType: "Promise<purify.Either<Error, number>>",
-    },
+    object: code`${methodNames.object}(identifier: ${objectType.identifierTypeAlias}): Promise<${sharedImports.Either}<Error, ${objectType.name}>`,
+    objectIdentifiers: code`${methodNames.objectIdentifiers}(query?: ${queryT}<${objectType.filterType}>): Promise<${sharedImports.Either}<Error, readonly ${objectType.identifierTypeAlias}[]>>`,
+    objects: code`${methodNames.objects}(query?: ${queryT}<${objectType.filterType}>): Promise<${sharedImports.Either}<Error, readonly ${objectType.name}[]>>`,
+    objectsCount: code`${methodNames.objectsCount}(query?: Pick<${queryT}<${objectType.filterType}>, "filter">): Promise<${sharedImports.Either}<Error, number>>`,
   };
 }
