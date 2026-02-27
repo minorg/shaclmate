926b585093d65c56d0b59f68c6eaa80de7fe1cd3
Minor Gordon <minorg@server.fake>
2026-02-26 20:05:31 -0500

    adjust shape counts

--- packages/shacl-ast/__tests__/ShapesGraph.test.ts
+++ packages/shacl-ast/__tests__/ShapesGraph.test.ts
@@ -4,8 +4,8 @@ import { testData } from "./testData.js";
 
 describe("ShapesGraph: kitchen sink", () => {
   it("should parse the shapes correctly", ({ expect }) => {
-    expect(testData.kitchenSink.shapesGraph.nodeShapes).toHaveLength(129);
-    expect(testData.kitchenSink.shapesGraph.propertyShapes).toHaveLength(140);
+    expect(testData.kitchenSink.shapesGraph.nodeShapes).toHaveLength(130);
+    expect(testData.kitchenSink.shapesGraph.propertyShapes).toHaveLength(156);
   });
 
   it("should parse ontologies correctly", ({ expect }) => {
