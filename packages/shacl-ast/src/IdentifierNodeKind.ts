c6920c5cdb7f6b00a7888051331f69fdc23d3c95
Minor Gordon <minorg@server.fake>
2026-02-28 14:12:23 -0500

    don't need IdentifierNodeKind.fromTermType

--- packages/shacl-ast/src/IdentifierNodeKind.ts
+++ packages/shacl-ast/src/IdentifierNodeKind.ts
@@ -4,9 +4,3 @@ import type { NodeKind } from "./NodeKind.js";
  * TypeScript enum corresponding to sh:NodeKind, for simpler manipulation.
  */
 export type IdentifierNodeKind = Exclude<NodeKind, "Literal">;
-
-export namespace IdentifierNodeKind {
-  export function fromTermType(termType: "BlankNode" | "NamedNode"): NodeKind {
-    return termType === "NamedNode" ? "IRI" : termType;
-  }
-}
