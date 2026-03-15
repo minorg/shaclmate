cd470e00c5d8c61deb05a245e638f5b68e183aaa
Minor Gordon <minorg@server.fake>
2026-03-15 18:13:49 -0400

    fixing tests

--- examples/kitchen-sink/__tests__/_testObjectSet/testObjectIdentifiersMethods.ts
+++ examples/kitchen-sink/__tests__/_testObjectSet/testObjectIdentifiersMethods.ts
@@ -1,5 +1,6 @@
 import dataFactory from "@rdfjs/data-model";
 import type { NamedNode } from "@rdfjs/types";
+import type * as kitchenSink from "@shaclmate/kitchen-sink-example";
 import { describe, it } from "vitest";
 import { data } from "./data.js";
 import type { ObjectSetFactory } from "./ObjectSetFactory.js";
@@ -18,116 +19,118 @@ export function testObjectIdentifiersMethods(
       const namedGraph1Iri = namedGraph1Object.$identifier as NamedNode;
       const namedGraph2Object = data.concreteChildClasses[3];
       const namedGraph2Iri = namedGraph2Object.$identifier as NamedNode;
-      const dataset = objectDataset({
+      const datasetObjects: Record<string, readonly kitchenSink.$Object[]> = {
         "": defaultGraphObjects,
-        "http://example.com/concreteChildClass2": [namedGraph1Object],
-        "http://example.com/concreteChildClass3": [namedGraph2Object],
-      });
+      };
+      datasetObjects[namedGraph1Iri.value] = [namedGraph1Object];
+      datasetObjects[namedGraph2Iri.value] = [namedGraph2Object];
+      const dataset = objectDataset(datasetObjects);
       const defaultGraphObjectSet = createObjectSet(dataset, {
         graph: dataFactory.defaultGraph(),
       });
       const namedGraph1ObjectSet = createObjectSet(dataset, {
-        graph: data.concreteChildClasses[3].$identifier,
+        graph: namedGraph1Iri,
       });
       // const namedGraph2ObjectSet = createObjectSet(dataset, {
-      //   graph: data.concreteChildClasses[3].$identifier,
+      //   graph: namedGraph2Iri,
       // });
       const unionGraphObjectSet = createObjectSet(dataset);
 
-      describe("objectIdentifiers", () => {
-        describe("default graph", () => {
-          it("no query", async ({ expect }) => {
-            expect(
-              (await defaultGraphObjectSet.concreteChildClassIdentifiers())
-                .unsafeCoerce()
-                .map((_) => _.value),
-            ).toStrictEqual(
-              defaultGraphObjects.map((_) => _.$identifier.value),
-            );
-          });
-
-          it("query of named graph", async ({ expect }) => {
-            expect(
-              (
-                await defaultGraphObjectSet.concreteChildClassIdentifiers({
-                  graph: namedGraph1Iri,
-                })
-              ).unsafeCoerce(),
-            ).toHaveLength(0);
-          });
+      describe("default graph", () => {
+        it("no query", async ({ expect }) => {
+          expect(
+            (await defaultGraphObjectSet.concreteChildClassIdentifiers())
+              .unsafeCoerce()
+              .map((_) => _.value)
+              .sort(),
+          ).toStrictEqual(
+            defaultGraphObjects.map((_) => _.$identifier.value).sort(),
+          );
         });
 
-        describe("named graph", () => {
-          it("no query", async ({ expect }) => {
-            expect(
-              (await namedGraph1ObjectSet.concreteChildClassIdentifiers())
-                .unsafeCoerce()
-                .map((_) => _.value),
-            ).toStrictEqual([namedGraph1Object.$identifier.value]);
-          });
+        it("query of named graph", async ({ expect }) => {
+          expect(
+            (
+              await defaultGraphObjectSet.concreteChildClassIdentifiers({
+                graph: namedGraph1Iri,
+              })
+            )
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual([namedGraph1Object.$identifier.value]);
+        });
+      });
 
-          it("query of different named graph", async ({ expect }) => {
-            expect(
-              (
-                await namedGraph1ObjectSet.concreteChildClassIdentifiers({
-                  graph: namedGraph2Iri,
-                })
-              ).unsafeCoerce(),
-            ).toHaveLength(0);
-          });
+      describe("named graph", () => {
+        it("no query", async ({ expect }) => {
+          expect(
+            (await namedGraph1ObjectSet.concreteChildClassIdentifiers())
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual([namedGraph1Object.$identifier.value]);
         });
 
-        describe("union graph", () => {
-          it("no query", async ({ expect }) => {
-            expect(
-              (await unionGraphObjectSet.concreteChildClassIdentifiers())
-                .unsafeCoerce()
-                .map((_) => _.value)
-                .sort(),
-            ).toStrictEqual(
-              [...defaultGraphObjects, namedGraph1Object, namedGraph2Object]
-                .map((_) => _.$identifier.value)
-                .sort(),
-            );
-          });
+        it("query of different named graph", async ({ expect }) => {
+          expect(
+            (
+              await namedGraph1ObjectSet.concreteChildClassIdentifiers({
+                graph: namedGraph2Iri,
+              })
+            )
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual([namedGraph2Object.$identifier.value]);
+        });
+      });
 
-          it("query of default graph", async ({ expect }) => {
-            expect(
-              (
-                await unionGraphObjectSet.concreteChildClassIdentifiers({
-                  graph: dataFactory.defaultGraph(),
-                })
-              )
-                .unsafeCoerce()
-                .map((_) => _.value),
-            ).toStrictEqual(
-              defaultGraphObjects.map((_) => _.$identifier.value),
-            );
-          });
+      describe("union graph", () => {
+        it("no query", async ({ expect }) => {
+          expect(
+            (await unionGraphObjectSet.concreteChildClassIdentifiers())
+              .unsafeCoerce()
+              .map((_) => _.value)
+              .sort(),
+          ).toStrictEqual(
+            [...defaultGraphObjects, namedGraph1Object, namedGraph2Object]
+              .map((_) => _.$identifier.value)
+              .sort(),
+          );
+        });
 
-          it("query of named graph 1", async ({ expect }) => {
-            expect(
-              (
-                await unionGraphObjectSet.concreteChildClassIdentifiers({
-                  graph: namedGraph1Iri,
-                })
-              )
-                .unsafeCoerce()
-                .map((_) => _.value),
-            ).toStrictEqual([namedGraph1Object.$identifier.value]);
-          });
+        it("query of default graph", async ({ expect }) => {
+          expect(
+            (
+              await unionGraphObjectSet.concreteChildClassIdentifiers({
+                graph: dataFactory.defaultGraph(),
+              })
+            )
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual(defaultGraphObjects.map((_) => _.$identifier.value));
+        });
 
-          it("query of named graph 2", async ({ expect }) => {
-            expect(
-              (
-                await unionGraphObjectSet.concreteChildClassIdentifiers({
-                  graph: namedGraph2Iri,
-                })
-              )
-                .unsafeCoerce()
-                .map((_) => _.value),
-            ).toStrictEqual([namedGraph2Object.$identifier.value]);
-          });
+        it("query of named graph 1", async ({ expect }) => {
+          expect(
+            (
+              await unionGraphObjectSet.concreteChildClassIdentifiers({
+                graph: namedGraph1Iri,
+              })
+            )
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual([namedGraph1Object.$identifier.value]);
+        });
+
+        it("query of named graph 2", async ({ expect }) => {
+          expect(
+            (
+              await unionGraphObjectSet.concreteChildClassIdentifiers({
+                graph: namedGraph2Iri,
+              })
+            )
+              .unsafeCoerce()
+              .map((_) => _.value),
+          ).toStrictEqual([namedGraph2Object.$identifier.value]);
         });
       });
     });
