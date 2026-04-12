008ea57e340688cc40ce3303f2e48cbb3570995e
Minor Gordon <minorg@server.fake>
2026-04-12 17:52:17 -0400

    bump version

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -4,7 +4,7 @@ import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
 
-const VERSION = "4.0.2";
+const VERSION = "4.0.3";
 
 const externalDependencies = {
   "@jsonforms/core": "3.5.1",
