5e92e6e1fc9f367e41cf4ceb27c9b3baa29c9fa0
Minor Gordon <minorg@server.fake>
2026-04-16 10:32:45 -0400

    start of ZodGenerator

---
+++ packages/compiler/src/generators/ts/ZodGenerator.ts
@@ -0,0 +1,53 @@
+import * as ast from "../../ast/index.js";
+import type { Generator } from "../Generator.js";
+// import { graphqlSchemaVariableStatement } from "./graphqlSchemaVariableStatement.js";
+// import { objectSetDeclarations } from "./objectSetDeclarations.js";
+import { snippets } from "./snippets.js";
+import { synthesizeUberObjectUnionType } from "./synthesizeUberObjectUnionType.js";
+// import { synthesizeUberObjectUnionType } from "./synthesizeUberObjectUnionType.js";
+import { TypeFactory } from "./TypeFactory.js";
+import { type Code, code, joinCode } from "./ts-poet-wrapper.js";
+
+export class ZodGenerator implements Generator {
+  private readonly typeFactory = new TypeFactory();
+
+  generate(ast_: ast.Ast): string {
+    const declarations: Code[] = [];
+
+    const objectTypesToposorted = ast.ObjectType.toposort(ast_.objectTypes).map(
+      (astObjectType) => this.typeFactory.createObjectType(astObjectType),
+    );
+
+    const objectUnionTypesToposorted = ast_.objectUnionTypes.map(
+      (astObjectUnionType) =>
+        this.typeFactory.createObjectUnionType(astObjectUnionType),
+    );
+
+    for (const objectType of objectTypesToposorted) {
+      declarations.push(objectType.declaration);
+    }
+    for (const objectUnionType of objectUnionTypesToposorted) {
+      declarations.push(objectUnionType.declaration);
+    }
+
+    const uberObjectUnionType = synthesizeUberObjectUnionType({
+      objectTypes: objectTypesToposorted.toReversed(), // Reverse topological order so children ane before parents
+    });
+    declarations.push(uberObjectUnionType.declaration);
+
+    declarations.splice(
+      0,
+      0,
+      joinCode(
+        Object.values(snippets)
+          .sort((left, right) =>
+            left.usageSiteName.localeCompare(right.usageSiteName),
+          )
+          .map((snippet) => code`${snippet.ifUsed}`),
+        { on: "\n\n" },
+      ),
+    );
+
+    return joinCode(declarations).toString({});
+  }
+}
