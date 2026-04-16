b08687ec2b080a3e22ebd8471c3452f5d584cb1b
Minor Gordon <minorg@server.fake>
2026-04-16 10:28:54 -0400

    break out compileTs function

--- packages/compiler/__tests__/TsGenerator.test.ts
+++ packages/compiler/__tests__/TsGenerator.test.ts
@@ -8,70 +8,10 @@ import {
 } from "@shaclmate/compiler";
 import ts from "typescript";
 import { describe, expect, it } from "vitest";
+import { compileTs } from "./compileTs.js";
 import { testData } from "./testData.js";
 
-const compilerOptions: ts.CompilerOptions = {
-  // @tsconfig/node18
-  // lib: ["ES2022"],
-  module: ts.ModuleKind.Node16,
-  moduleResolution: ts.ModuleResolutionKind.Node16,
-  target: ts.ScriptTarget.ES2022,
-
-  // @tsconfig/strictest
-  strict: true,
-  allowUnusedLabels: false,
-  allowUnreachableCode: false,
-  // exactOptionalPropertyTypes: true,
-  noFallthroughCasesInSwitch: true,
-  noImplicitOverride: true,
-  noImplicitReturns: true,
-  noPropertyAccessFromIndexSignature: true,
-  // noUncheckedIndexedAccess: true,
-  noUnusedLocals: true,
-  noUnusedParameters: true,
-  isolatedModules: true,
-  esModuleInterop: true,
-  skipLibCheck: true,
-};
-
 const thisDirectoryPath = path.dirname(fileURLToPath(import.meta.url));
-
-function compile(source: string, sourceDirectoryPath?: string): void {
-  const host = ts.createCompilerHost(compilerOptions, true);
-
-  const generatedFilePath = path.join(
-    sourceDirectoryPath ?? thisDirectoryPath,
-    "generated.ts",
-  );
-  host.getCurrentDirectory = () => thisDirectoryPath;
-  host.getSourceFile = (fileName, languageVersion) => {
-    if (fileName === generatedFilePath) {
-      return ts.createSourceFile(fileName, source, languageVersion);
-    }
-    return ts.sys.readFile(fileName)
-      ? ts.createSourceFile(
-          fileName,
-          ts.sys.readFile(fileName)!,
-          languageVersion,
-        )
-      : undefined;
-  };
-
-  // Capture output in memory
-  const outputs = new Map<string, string>();
-  host.writeFile = (fileName, content) => {
-    outputs.set(fileName, content);
-  };
-
-  const program = ts.createProgram([generatedFilePath], compilerOptions, host);
-  const emitResult = program.emit();
-  const diagnostics = emitResult.diagnostics
-    .concat(ts.getPreEmitDiagnostics(program))
-    .filter(
-      (diagnostic) => diagnostic.category !== 1 || diagnostic.code !== 6133,
-    ); // Ignore unused code
-  expect(diagnostics).toHaveLength(0);
-}
 
 function generate(parameters: {
   iriPrefixMap: PrefixMap;
@@ -129,7 +69,11 @@ describe("TsGenerator", () => {
       //   return;
       // }
 
-      compile(generate(shapesGraphEither.unsafeCoerce()), sourceDirectoryPath);
+      const diagnostics = compileTs(
+        generate(shapesGraphEither.unsafeCoerce()),
+        sourceDirectoryPath,
+      );
+      expect(diagnostics).toHaveLength(0);
     }, 60000);
   }
 
