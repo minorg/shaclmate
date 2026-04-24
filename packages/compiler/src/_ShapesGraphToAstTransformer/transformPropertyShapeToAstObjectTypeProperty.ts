8767cd62006c4d8153a7c2789d749348c8cf04d3
Minor Gordon <minorg@server.fake>
2026-04-24 15:31:39 -0400

    fix property name inference

--- packages/compiler/src/_ShapesGraphToAstTransformer/transformPropertyShapeToAstObjectTypeProperty.ts
+++ packages/compiler/src/_ShapesGraphToAstTransformer/transformPropertyShapeToAstObjectTypeProperty.ts
@@ -59,7 +59,7 @@ function propertyName(
   propertyShape: input.PropertyShape,
 ): string {
   // Explicit shaclmate:name or sh:name
-  const name = propertyShape.name.alt(propertyShape.name).extract();
+  const name = List.head(propertyShape.names).alt(propertyShape.name).extract();
   if (name) {
     return name;
   }
