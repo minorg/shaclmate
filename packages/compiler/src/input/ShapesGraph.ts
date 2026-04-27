59248e1cd5090f4b2f3d41d08bcf63e09858c5e2
Minor Gordon <minorg@server.fake>
2026-04-27 12:22:27 -0400

    ShapesGraph: comment out extra fields

--- packages/compiler/src/input/ShapesGraph.ts
+++ packages/compiler/src/input/ShapesGraph.ts
@@ -2,76 +2,72 @@ import {
   ShapesGraph as _ShapesGraph,
   type CurieFactory,
 } from "@shaclmate/shacl-ast";
-import { owl, rdf, rdfs } from "@tpluscode/rdf-ns-builders";
 import type { Either } from "purify-ts";
 import type { Resource } from "rdfjs-resource";
-import { ancestorClassIris } from "./ancestorClassIris.js";
-import { descendantClassIris } from "./descendantClassIris.js";
 import * as generated from "./generated.js";
-import type { NodeShape } from "./NodeShape.js";
-import type { Shape } from "./Shape.js";
 
 export type ShapesGraph = _ShapesGraph<
-  NodeShape,
+  generated.NodeShape,
   generated.Ontology,
   generated.PropertyGroup,
   generated.PropertyShape,
-  Shape
+  generated.Shape
 >;
 
 export namespace ShapesGraph {
   class Factory extends _ShapesGraph.Factory<
-    NodeShape,
+    generated.NodeShape,
     generated.Ontology,
     generated.PropertyGroup,
     generated.PropertyShape,
-    Shape
+    generated.Shape
   > {
     protected override createNodeShape({
       resource,
     }: {
       resource: Resource;
-    }): Either<Error, NodeShape> {
+    }): Either<Error, generated.NodeShape> {
       return generated.NodeShape.$fromRdfResource(resource, {
         ignoreRdfType: true,
         preferredLanguages: this.preferredLanguages,
-      }).map((generatedShape) => {
-        let isClass =
-          resource.isInstanceOf(owl.Class) || resource.isInstanceOf(rdfs.Class);
-
-        const isList = resource.isSubClassOf(rdf.List);
-        if (isList) {
-          isClass = true; // RDFS entailment: if A rdfs:subClassOf rdf:List then A is an rdfs:Class
-        }
-
-        const ancestorClassIris_ = ancestorClassIris(
-          resource,
-          Number.MAX_SAFE_INTEGER,
-        ).filter((ancestorClassIri) => !ancestorClassIri.equals(rdf.List));
-        if (ancestorClassIris_.length > 0) {
-          isClass = true; // RDFS entailment: if A rdfs:subClassOf B then both A and B are rdfs:Class's
-        }
-
-        const descendantClassIris_ = descendantClassIris(
-          resource,
-          Number.MAX_SAFE_INTEGER,
-        );
-        if (descendantClassIris_.length > 0) {
-          isClass = true; // RDFS entailment, see above
-        }
-
-        return {
-          ...generatedShape,
-          ancestorClassIris: ancestorClassIris_,
-          childClassIris: descendantClassIris(resource, 1),
-          descendantClassIris: descendantClassIris_,
-          isClass,
-          isList,
-          parentClassIris: ancestorClassIris(resource, 1).filter(
-            (ancestorClassIri) => !ancestorClassIri.equals(rdf.List),
-          ),
-        };
       });
+      // .map((generatedShape) => {
+      //   let isClass =
+      //     resource.isInstanceOf(owl.Class) || resource.isInstanceOf(rdfs.Class);
+
+      //   const isList = resource.isSubClassOf(rdf.List);
+      //   if (isList) {
+      //     isClass = true; // RDFS entailment: if A rdfs:subClassOf rdf:List then A is an rdfs:Class
+      //   }
+
+      //   const ancestorClassIris_ = ancestorClassIris(
+      //     resource,
+      //     Number.MAX_SAFE_INTEGER,
+      //   ).filter((ancestorClassIri) => !ancestorClassIri.equals(rdf.List));
+      //   if (ancestorClassIris_.length > 0) {
+      //     isClass = true; // RDFS entailment: if A rdfs:subClassOf B then both A and B are rdfs:Class's
+      //   }
+
+      //   const descendantClassIris_ = descendantClassIris(
+      //     resource,
+      //     Number.MAX_SAFE_INTEGER,
+      //   );
+      //   if (descendantClassIris_.length > 0) {
+      //     isClass = true; // RDFS entailment, see above
+      //   }
+
+      //   return {
+      //     ...generatedShape,
+      //     ancestorClassIris: ancestorClassIris_,
+      //     childClassIris: descendantClassIris(resource, 1),
+      //     descendantClassIris: descendantClassIris_,
+      //     isClass,
+      //     isList,
+      //     parentClassIris: ancestorClassIris(resource, 1).filter(
+      //       (ancestorClassIri) => !ancestorClassIri.equals(rdf.List),
+      //     ),
+      //   };
+      // });
     }
 
     protected override createOntology({
