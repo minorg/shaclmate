c11dafd35175731e7560fbd3e46b85fac8d1234d
Minor Gordon <minorg@server.fake>
2026-05-04 16:27:21 -0400

    fix pino-pretty use

--- apps/cli/src/logger.ts
+++ apps/cli/src/logger.ts
@@ -1,18 +1,16 @@
 import { pino } from "pino";
 
-export const logger = pino(
-  {
-    level:
-      process.env["NODE_ENV"] === "development" ||
-      process.env["NODE_ENV"] === "test"
-        ? "debug"
-        : "info",
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-      },
+export const logger = pino({
+  level:
+    process.env["NODE_ENV"] === "development" ||
+    process.env["NODE_ENV"] === "test"
+      ? "debug"
+      : "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      destination: 2,
+      colorize: true,
     },
   },
-  (pino as any)["destination"] ? (pino as any).destination(2) : undefined,
-);
+});
