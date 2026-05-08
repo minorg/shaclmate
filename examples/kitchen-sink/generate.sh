1a574288371e9fb0fde2352a5e4a5815e3dd1809
Minor Gordon <minorg@server.fake>
2026-05-08 15:34:59 -0400

    change generate.sh to avoid formatting the file, having blank node issues with rapper

--- examples/kitchen-sink/generate.sh
+++ examples/kitchen-sink/generate.sh
@@ -4,7 +4,9 @@ set -e
 
 cd "$(dirname "$0")"
 
-rapper -i turtle -o turtle -q src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
+# rapper is stumbling on blank nodes when re-formatting the kitchen sink example
+# rapper -i turtle -o turtle -q src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
+# riot --formatted=TURTLE src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
 ../../apps/cli/dist/cli.js generate ast-json src/kitchen-sink.shaclmate.ttl | sponge src/ast.json
 time ../../apps/cli/dist/cli.js generate ts src/kitchen-sink.shaclmate.ttl | sponge src/generated.ts
 npm exec biome -- check --write src/generated.ts
