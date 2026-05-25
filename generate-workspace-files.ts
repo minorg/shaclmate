0e9ad2f149c478cdb67571cebd8056142c6b5ec6
Minor Gordon <minorg@server.fake>
2026-05-25 18:21:50 -0400

    bump version

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import url from "node:url";
 import type { CompilerOptions } from "typescript";
 
-const VERSION = "4.0.39";
+const VERSION = "4.0.40";
 
 const rdfxVersion = "0.0.18";
 const vitestVersion = "~4.1.5";
