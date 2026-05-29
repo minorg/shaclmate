1231b8a7558c8cc01a9e1e47a7d7174324865080
Minor Gordon <minorg@server.fake>
2026-05-29 11:59:04 -0400

    adding tests

--- packages/shacl-ast/__tests__/Shape.test.ts
+++ packages/shacl-ast/__tests__/Shape.test.ts
@@ -3,8 +3,11 @@ import { beforeAll, describe, it } from "vitest";
 import type { Shape } from "../src/generated.js";
 import { testData } from "./testData.js";
 import "@rdfx/testing";
+import { LiteralFactory } from "@rdfx/literal";
 import type { ShapesGraph } from "../src/ShapesGraph.js";
 import { ex } from "./namespaces.js";
+
+const literalFactory = new LiteralFactory({ dataFactory });
 
 describe("Shape", () => {
   const shapesGraphEither = testData.shapesGraphs.wellFormed.syntax;
@@ -78,8 +81,43 @@ describe("Shape", () => {
         expect(shape.flags.extract()).toStrictEqual("flags");
       });
 
+      it("in", ({ expect }) => {
+        const in_ = shape.in_.extract()!;
+        expect(in_).toHaveLength(2);
+        expect(in_[0].equals(ex("in1")));
+        expect(in_[1].equals(ex("in2")));
+      });
+
       it("label", ({ expect }) => {
         expect(shape.label.extract()).toStrictEqual("label");
+      });
+
+      it("languageIn", ({ expect }) => {
+        expect(shape.languageIn.extract()).toEqual(["en", "fr"]);
+      });
+
+      it("maxExclusive", ({ expect }) => {
+        expect(shape.maxExclusive.extract()?.equals(literalFactory.number(1)));
+      });
+
+      it("maxInclusive", ({ expect }) => {
+        expect(shape.maxInclusive.extract()?.equals(literalFactory.number(1)));
+      });
+
+      it("maxLength", ({ expect }) => {
+        expect(shape.maxLength.extract()).toStrictEqual(1n);
+      });
+
+      it("minExclusive", ({ expect }) => {
+        expect(shape.minExclusive.extract()?.equals(literalFactory.number(0)));
+      });
+
+      it("minInclusive", ({ expect }) => {
+        expect(shape.minInclusive.extract()?.equals(literalFactory.number(0)));
+      });
+
+      it("minLength", ({ expect }) => {
+        expect(shape.minLength.extract()).toStrictEqual(0n);
       });
     });
   }
