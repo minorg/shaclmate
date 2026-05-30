3d879c7d01b3082846e127b12720b361caa8f7c2
Minor Gordon <minorg@server.fake>
2026-05-30 15:18:47 -0400

    remove class hierarchy from schema test

--- examples/kitchen-sink/__tests__/schema.test.ts
+++ examples/kitchen-sink/__tests__/schema.test.ts
@@ -3,27 +3,6 @@ import { describe, it } from "vitest";
 
 describe("schema", () => {
   describe("properties", () => {
-    it("should have an own property", ({ expect }) => {
-      expect(
-        kitchenSink.ClassHierarchy3.schema.properties.classHierarchy3Property
-          .path.value,
-      ).toStrictEqual("http://example.com/classHierarchy3Property");
-    });
-
-    it("should have a parent property", ({ expect }) => {
-      expect(
-        kitchenSink.ClassHierarchy3.schema.properties.classHierarchy2Property
-          .path.value,
-      ).toStrictEqual("http://example.com/classHierarchy2Property");
-    });
-
-    it("should have an ancestor property", ({ expect }) => {
-      expect(
-        kitchenSink.ClassHierarchy3.schema.properties.classHierarchy0Property
-          .path.value,
-      ).toStrictEqual("http://example.com/classHierarchy0Property");
-    });
-
     it("union properties should have a common parent property", ({
       expect,
     }) => {
