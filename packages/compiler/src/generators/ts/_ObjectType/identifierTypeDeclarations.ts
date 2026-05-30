80d5ad315203500d94515d22f0e51637384494b3
Minor Gordon <minorg@server.fake>
2026-05-30 12:51:25 -0400

    ts ObjectType: don't reuse identifier type declarations

--- packages/compiler/src/generators/ts/_ObjectType/identifierTypeDeclarations.ts
+++ packages/compiler/src/generators/ts/_ObjectType/identifierTypeDeclarations.ts
@@ -1,29 +1,9 @@
-import { codeEquals } from "../codeEquals.js";
 import type { ObjectType } from "../ObjectType.js";
 import { type Code, code } from "../ts-poet-wrapper.js";
 
 export function identifierTypeDeclarations(this: ObjectType): readonly Code[] {
   if (!this.configuration.features.has("Object.type")) {
     return [];
-  }
-
-  const ancestorObjectTypeWithSameIdentifierType =
-    this.ancestorObjectTypes.find(
-      (ancestorObjectType) =>
-        ancestorObjectType.identifierType.kind === this.identifierType.kind &&
-        codeEquals(
-          ancestorObjectType.identifierType.expression,
-          this.identifierType.expression,
-        ),
-    );
-
-  if (ancestorObjectTypeWithSameIdentifierType) {
-    // This object type's identifier type has the same identifier type as an ancestor object type,
-    // so just reuse the latter.
-    return [
-      code`export type Identifier = ${ancestorObjectTypeWithSameIdentifierType.identifierTypeAlias};`,
-      code`export const Identifier = ${ancestorObjectTypeWithSameIdentifierType.identifierTypeAlias};`,
-    ];
   }
 
   // Bespoke identifier type and associated functions
