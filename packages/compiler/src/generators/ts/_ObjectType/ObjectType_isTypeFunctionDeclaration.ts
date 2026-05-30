3480f843ff86c4b9d559dbc7075b12d06383e12c
Minor Gordon <minorg@server.fake>
2026-05-30 12:55:42 -0400

    adjust isType

--- packages/compiler/src/generators/ts/_ObjectType/ObjectType_isTypeFunctionDeclaration.ts
+++ packages/compiler/src/generators/ts/_ObjectType/ObjectType_isTypeFunctionDeclaration.ts
@@ -1,6 +1,6 @@
 import { Maybe } from "purify-ts";
 import type { ObjectType } from "../ObjectType.js";
-import { type Code, code } from "../ts-poet-wrapper.js";
+import { type Code, code, literalOf } from "../ts-poet-wrapper.js";
 
 export function ObjectType_isTypeFunctionDeclaration(
   this: ObjectType,
@@ -11,11 +11,6 @@ export function ObjectType_isTypeFunctionDeclaration(
 
   return Maybe.of(code`\
 export function is${this.alias.unsafeCoerce()}(object: ${this.configuration.syntheticNamePrefix}Object): object is ${this.alias.unsafeCoerce()} {
-  switch (object.${this._discriminantProperty.name}) {
-    ${this._discriminantProperty.type.descendantValues
-      .concat(this._discriminantProperty.type.ownValues)
-      .map((value) => `case "${value}":`)
-      .join("\n")} return true; default: return false;
-  }
+  return ${this._discriminantProperty.name} === ${literalOf(this._discriminantProperty.value)};
 }`);
 }
