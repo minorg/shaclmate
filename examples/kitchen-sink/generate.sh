56603051da0823603459344606f2bebdde2941bb
Minor Gordon <minorg@server.fake>
2026-05-31 15:27:39 -0400

    reformat kitchen sink ttl

--- examples/kitchen-sink/generate.sh
+++ examples/kitchen-sink/generate.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 # rapper is stumbling on blank nodes when re-formatting the kitchen sink example
-# rapper -i turtle -o turtle -q src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
+rapper -i turtle -o turtle -q src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
 # riot --formatted=TURTLE src/kitchen-sink.shaclmate.ttl | sponge src/kitchen-sink.shaclmate.ttl
 ../../apps/cli/dist/cli.js generate ast-json src/kitchen-sink.shaclmate.ttl | sponge src/ast.json
 time ../../apps/cli/dist/cli.js generate ts --feature object.equals --feature object.hash --feature json --feature rdf --feature sparql src/kitchen-sink.shaclmate.ttl | sponge src/generated.ts
