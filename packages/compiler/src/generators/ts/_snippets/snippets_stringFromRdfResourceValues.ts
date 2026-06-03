b13c0e4af7c4586e9fa5396b589f9372fa269622
Minor Gordon <minorg@server.fake>
2026-06-03 19:41:44 -0400

    stringFromRdfResourceValues: use termLikeFromRdfResourceValues

--- packages/compiler/src/generators/ts/_snippets/snippets_stringFromRdfResourceValues.ts
+++ packages/compiler/src/generators/ts/_snippets/snippets_stringFromRdfResourceValues.ts
@@ -9,12 +9,7 @@ export const snippets_stringFromRdfResourceValues: SnippetFactory = ({
   conditionalOutput(
     `${syntheticNamePrefix}stringFromRdfResourceValues`,
     code`\
-function ${syntheticNamePrefix}stringFromRdfResourceValues<T extends string>(values: ${imports.Resource}.Values, { preferredLanguages, propertySchema }: Parameters<${snippets.FromRdfResourceValuesFunction}<T, ${snippets.StringSchema}<T>>>[1]): ${imports.Either}<Error, ${imports.Resource}.Values<T>> {
-  let chain = ${imports.Either}.of(values);
-  if (schema.type.languageIn && schema.type.languageIn.length > 0) {
-    chain = chain.chain(values => ${snippets.fromRdfLanguageIn}(values, schema.type.languageIn));
-  }
-  chain = chain.chain(values => ${snippets.fromRdfPreferredLanguages}(values, preferredLanguages));
-  return chain.chain(values => values.chainMap(value => value.toString(schema.in)));
+function ${syntheticNamePrefix}stringFromRdfResourceValues<T extends string>(values: ${imports.Resource}.Values, options: Parameters<${snippets.FromRdfResourceValuesFunction}<T, ${snippets.StringSchema}<T>>>[1]): ${imports.Either}<Error, ${imports.Resource}.Values<T>> {
+  return ${snippets.termLikeFromRdfResourceValues}(values, options).chain(values => values.chainMap(value => value.toString(options.propertySchema.type.in)));
 }`,
   );
