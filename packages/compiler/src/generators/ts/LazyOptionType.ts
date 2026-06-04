6b5f689c48c9b53e657a21e48fc7df05be2a9959
Minor Gordon <minorg@server.fake>
2026-06-04 14:29:04 -0400

    lazy type functions

--- packages/compiler/src/generators/ts/LazyOptionType.ts
+++ packages/compiler/src/generators/ts/LazyOptionType.ts
@@ -63,12 +63,18 @@ export class LazyOptionType extends Super {
     return code`${this.partialType.fromJsonExpression(parameters)}.map(partial => new ${this.runtimeClass.name}({ ${this.runtimeClass.partialPropertyName}: partial, resolver: (partial) => Promise.resolve(${this.reusables.imports.Left}(new Error(\`unable to resolve \${partial} deserialized from JSON\`))) }))`;
   }
 
-  // override fromRdfResourceValuesExpression(
-  //   parameters: Parameters<Super["fromRdfResourceValuesExpression"]>[0],
-  // ): Code {
-  //   const { variables } = parameters;
-  //   return code`${this.partialType.fromRdfResourceValuesExpression(parameters)}.map(values => values.map(${this.runtimeClass.partialPropertyName} => new ${this.runtimeClass.name}({ ${this.runtimeClass.partialPropertyName}, resolver: (partial, options) => ${variables.objectSet}.${this.resolveType.itemType.objectSetMethodNames.object}(partial.${this.configuration.syntheticNamePrefix}identifier(), options) })))`;
-  // }
+  @Memoize()
+  override get fromRdfResourceValuesFunction(): Code {
+    return code`\
+(
+    (
+      (values, { objectSet, schema, ...otherOptions }) =>
+        ${this.partialType.fromRdfResourceValuesFunction}(values, { ...otherOptions, objectSet, schema: schema.partialType })
+          .map(values => values.map(${this.runtimeClass.partialPropertyName} => new ${this.runtimeClass.name}({ ${this.runtimeClass.partialPropertyName}, resolver: (partial, options) => objectSet.${this.resolveType.itemType.objectSetMethodNames.object}(partial.${this.configuration.syntheticNamePrefix}identifier(), options) })))
+    )
+    satisfies ${this.reusables.snippets.FromRdfResourceValuesFunction}<${this.expression}, ${this.schemaType}>
+)`;
+  }
 
   override graphqlResolveExpression({
     variables,
