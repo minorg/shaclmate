9937cc6ee20a24cc5b3358a65d2f9acbeca75650
Minor Gordon <minorg@server.fake>
2026-06-04 21:04:53 -0400

    implement termFromRdfResourceValues properly

--- packages/compiler/src/generators/ts/_snippets/snippets_termFromRdfResourceValues.ts
+++ packages/compiler/src/generators/ts/_snippets/snippets_termFromRdfResourceValues.ts
@@ -10,6 +10,17 @@ export const snippets_termFromRdfResourceValues: SnippetFactory = ({
     `${syntheticNamePrefix}termFromRdfResourceValues`,
     code`\
 function ${syntheticNamePrefix}termFromRdfResourceValues<T extends ${imports.BlankNode} | ${imports.Literal} | ${imports.NamedNode}>(values: ${imports.Resource}.Values, options: Parameters<${snippets.FromRdfResourceValuesFunction}<T, ${snippets.TermSchema}<T>>>[1]): ${imports.Either}<Error, ${imports.Resource}.Values<T>> {
-  return ${snippets.termLikeFromRdfResourceValues}(values, options).chain(values => values.chainMap(value => value.toTerm()));
+  const { focusResource, propertyPath, schema } = options;
+  return ${snippets.termLikeFromRdfResourceValues}(values, options).chain(values => values.chainMap(value => value.toTerm().chain(term => {
+    if (schema.in && schema.in.length > 0 && !schema.in.some(in_ => in_.equals(term))) {
+      return ${imports.Left}(new ${imports.Resource}.MistypedTermValueError({ actualValue: term, expectedValueType: "Term in", focusResource, propertyPath }));
+    }
+
+    if (!schema.types.some(type => term.termType === type)) {
+      return ${imports.Left}(new ${imports.Resource}.MistypedTermValueError({ actualValue: term, expectedValueType: "Term types", focusResource, propertyPath }));
+    }
+
+    return ${imports.Right}(term as T);
+  })));
 }`,
   );
