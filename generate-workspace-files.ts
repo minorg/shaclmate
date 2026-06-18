3fc8b1b95fb2136acab735ea07f14d2c6d2caa7b
Minor Gordon <minorg@server.fake>
2026-06-18 14:38:21 -0400

    bump rdfx version and version

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -5,9 +5,9 @@ import path from "node:path";
 import url from "node:url";
 import type { CompilerOptions } from "typescript";
 
-const VERSION = "4.0.56";
+const VERSION = "4.0.57";
 
-const rdfxVersion = "0.0.26";
+const rdfxVersion = "0.0.27";
 const vitestVersion = "~4.1.5";
 
 const externalDependencies = {
