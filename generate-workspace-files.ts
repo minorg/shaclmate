26d2e183f83d7172bdfe5b63b038c6e1178afcdf
Minor Gordon <minorg@server.fake>
2026-07-07 11:32:54 -0400

    add to rdfjsTermExpression

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import url from "node:url";
 import type { CompilerOptions } from "typescript";
 
-const VERSION = "4.0.67";
+const VERSION = "4.0.68";
 
 const rdfxVersion = "0.0.32";
 const vitestVersion = "~4.1.5";
--- packages/compiler/src/generators/ts/rdfjsTermExpression.ts
+++ packages/compiler/src/generators/ts/rdfjsTermExpression.ts
@@ -56,6 +56,7 @@ export function rdfjsTermExpression(
           case "float":
           case "date":
           case "dateTime":
+          case "dateTimeStamp":
           case "decimal":
           case "double":
           case "int":
